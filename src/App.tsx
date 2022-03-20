633e42988e0e008562e54ad2d92a51ff9593f992
Andrey <55235312+andrew133@users.noreply.github.com>
2022-03-20 21:04:54 +0300

    20

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export type filterType = 'All'| 'Active'|'Completed'
 function App() {
 
     const [tasks, setTasks] = useState<Array<TaskType>>([
-        {id: v1(), title: "HTML&CSS", isDone: true},
+        {id: v1(), title: "HTML&CS", isDone: true},
         {id: v1(), title: "JS", isDone: true},
         {id: v1(), title: "ReactJS", isDone: false},
         {id: v1(), title: "Rest A", isDone: false},
